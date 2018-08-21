f0cc1fcc68263fa3b14b00c426c10ae563abf71e
CaitlinCodes <caiit@me.com>
2018-08-21 15:23:50 -0600

    routes and index

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ let server = express()
 let bp = require('body-parser')
 let port = 3000
 
+require('./server-assets/db/mlab-config')
+
 server.use(bp.json())
 server.use(bp.urlencoded({
   extended: true
 }))
+
+let carRoutes = require('./server-assets/routes/car-routes')
+
+server.use('api/car', carRoutes)
 
 server.listen(port, () => {
   console.log('Running on Port:', port)
