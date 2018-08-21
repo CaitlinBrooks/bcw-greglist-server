d6568a54b3c4758642915877d33975d188435170
CaitlinCodes <caiit@me.com>
2018-08-21 14:03:47 -0600

    car model

---
+++ server-assets/models/Car.js
@@ -0,0 +1,23 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+let schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'No description included'
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+})
+
+// What if I wanted to include a required image of the car?
+// image: Image,
+// required: true
+
+module.exports = mongoose.model('Car', schema)
